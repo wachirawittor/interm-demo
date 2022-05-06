7a84fb1d09ae21df60224761d4fcaefacebe1810
wachirawittor <58878197+wachirawittor@users.noreply.github.com>
2022-05-06 10:28:20 +0700

    test commit files

---
+++ internship/test/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.err.println("sheeeesh");
+        System.out.println("codeeee");
+    }
+}
